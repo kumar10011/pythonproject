8194eb07ed673e6327907fbe174597a5a8a16967
kumar10011 <150844327+kumar10011@users.noreply.github.com>
2025-10-28 10:22:33 +0530

    Create main.tf

---
+++ terrafrom-setup/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+region = var.region
+profile = "terraform-user"
+}
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0360c520857e3138f"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "DevOpsAppServer"
+  }
+}
